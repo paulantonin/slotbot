7acf83c6c3e46a7c707154a2389eded27817d289
Paul-Antonin <paul-antonin.larrieu@student-cs.com>
2026-02-20 13:45:02 +0100

    import sqlite

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import asyncio
+import sqlite3
 from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
